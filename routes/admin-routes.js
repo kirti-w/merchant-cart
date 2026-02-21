2c5435124833f13cdd37d51d7e161b5c9b27fac2
Kirti Wadhwani <kwadhwani@36eighttechnologies.com>
2026-02-21 08:05:24 -0500

    removed extra code

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,7 +4,6 @@ import * as cartDB from "../cartModule.js";
 import { Order, Product } from "../models/index.js";
 
 const router = express.Router();
-const isAdmin = true;
 
 router.get("/products", async (req, res) => {
   try {
